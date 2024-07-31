a2b354cd51ce445e56b6613048a362e870fd6d4d
Mawi <mark@example.com>
2024-07-31 14:39:37 +0800

    Remove deleted files

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const serverless = require("serverless-http");
 const router = require("./routes/auth");
@@ -25,31 +25,3 @@ app.use("/.netlify/functions/api", router);
 app.use("/.netlify/functions/api/driver", driverRouter);
 app.use("/.netlify/functions/api/ride", rideRouter);
 module.exports.handler = serverless(app);
-=======
-const express = require('express');
-const serverless = require('serverless-http');
-const router = require('./routes/recipe');
-const mongoose = require('mongoose');
-const cors =  require('cors');
-
-const app = express();
-
-// your mongoDB Cloud URL
-
-const dbCloudUrl =
-'mongodb+srv://Mawi:Mawi21@cluster0.twni9tv.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0'; // your mongoDB Cloud URL
-
-const dbLocalUrl = 'mongodb://localhost:27017/authors';
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-mongoose
-.connect(dbCloudUrl || dbLocalUrl)
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Failed to connect to MongoDB', error));
-
-app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
->>>>>>> 591c60e6ba13decd0ec21eb9e31a05bf75f2141d
--- functions/models/recipe.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const recipeSchema = require('../schema/recipe');
-
-const RecipeModel = mongoose.model('Recipe', recipeSchema);
-
-module.exports = RecipeModel;   
