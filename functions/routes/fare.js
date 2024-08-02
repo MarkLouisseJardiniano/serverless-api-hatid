990aba93f43717a78502ce8dfb1d20f708c45538
Mawi <mark@example.com>
2024-08-02 14:20:48 +0800

    add edit-fare in routes

--- functions/routes/fare.js
+++ functions/routes/fare.js
@@ -2,8 +2,20 @@ const express = require("express");
 const router = express.Router();
 const Fare = require("../schema/fare");
 
+
+
+router.get('/fares', async (req, res) => {
+    try {
+      const fares = await Fare.find();
+      res.json(fares);
+    } catch (err) {
+      console.error('Error fetching fares:', err);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  });
+  
 // Create or update fare for a vehicle type
-router.post("/admin-fare", async (req, res) => {
+router.post("/set-fare", async (req, res) => {
   const { vehicleType, baseFare, farePerKm, bookingFee } = req.body;
 
   if (!vehicleType || baseFare === undefined || farePerKm === undefined || bookingFee === undefined) {
@@ -22,5 +34,22 @@ router.post("/admin-fare", async (req, res) => {
     res.status(500).json({ error: "Error setting fare" });
   }
 });
+
+
+router.put("/edit-fare/:id", async (req,res) => {
+    try {
+        const fareId = req.params.id
+        const { baseFare, farePerKm, bookingFee } = req.body;
+    
+        const updateFare = await Fare.findByIdAndUpdate(fareId, {baseFare, farePerKm, bookingFee}, {new: true} );
+        if (!updateFare) {
+    return res.status(404).json({message: "Fare not Found"})
+        }
+        res.status(200).json({ status: 'ok', data: updateFare });
+    } catch (error) {
+        console.error('Error updating fare data:', error);
+        res.status(500).json({ message: 'Server Error' });
+      }
+})
 
 module.exports = router;
