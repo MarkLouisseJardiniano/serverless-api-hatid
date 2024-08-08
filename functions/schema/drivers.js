e6f4e1f152abf99870cf335c5a13e29e143d0585
Mawi <mark@example.com>
2024-08-08 12:56:37 +0800

    complete the signup form for driver

--- functions/schema/drivers.js
+++ functions/schema/drivers.js
@@ -1,34 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const driverSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   number: {
     type: String,
     required: true,
-    match: [/^\d{7,15}$/, 'Phone number must be between 7 and 15 digits'],
+    match: [/^\d{7,15}$/, "Phone number must be between 7 and 15 digits"],
   },
   birthday: {
     type: String,
-    required: true
+    required: true,
   },
   address: {
     type: String,
-    required: true
+    required: true,
   },
+  accountType: {
+    type: String, 
+    required: true
+  }
 });
 
-const Driver = mongoose.model('Driver', driverSchema);
+const Driver = mongoose.model("Driver", driverSchema);
 
 module.exports = Driver;
