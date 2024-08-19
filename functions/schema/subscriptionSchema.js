d2ce55ec52b43340cdab9335bb4c0461f72cacdc
Mawi <mark@example.com>
2024-08-19 16:53:07 +0800

    add subscription]

--- functions/schema/subscriptionSchema.js
+++ functions/schema/subscriptionSchema.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 
 const subscriptionSchema = new mongoose.Schema({
-  driver : {
+  driver: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Driver',
+    required: true
   },
   subscriptionType: {
     type: String,
     required: true,
     enum: ["Free", "Monthly", "Quarterly", "Annually"]
   }
-
 });
 
-module.exports = subscriptionSchema;
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+module.exports = Subscription;
