a403a87549056c92a9efd187538ed93753137718
Mawi <mark@example.com>
2024-08-19 16:27:53 +0800

    add subscription]

--- functions/routes/subscription.js
+++ functions/routes/subscription.js
@@ -1,41 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const Subscription = require("../schema/subscriptionSchema");
+const Driver = require("../schema/drivers");
+
 
 router.get("/subscription", async (req, res) => {
-    try {
-      const subscription = await Subs.find();
-      res.json(subscription);
-    } catch (err) {
-      console.error("Error fetching subscription:", err);
-      res.status(500).json({ message: "Server Error" });
+  try {
+    const subscriptions = await Subscription.find();
+    res.json(subscriptions);
+  } catch (err) {
+    console.error("Error fetching subscriptions:", err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+
+router.post("/subscription", async (req, res) => {
+  try {
+    const { driverId, subscriptionType } = req.body;
+
+    if (!driverId) {
+      return res.status(400).json({ message: "DriverId not found" });
     }
-  });
 
-router.post("/subscription", async (req,res) => {
-    try {
-        const {driverId, subscriptionType} = req.body;
-
-        if(!driverId) {
-            return res.status(200).json({message: "DriverId nt found"})
-        }
-
-        const driver =  await Subscription.findById(driverId);
-        if(!driver) {
-            return res.status(400).json({message: "Driver not found haha"})
-        }
-
-        const newSubscription = new Subscription({
-            driver: driverId,
-            subscriptionType
-        });
-
-        await newSubscription.save();
-        res.status(201).json(newSubscription)
-    } catch (error) {
-        console.error("Error creating subs:", error);
-        res.status(500).json({ error: "Error creating subs" });
+    const driver = await Driver.findById(driverId);
+    if (!driver) {
+      return res.status(400).json({ message: "Driver not found" });
     }
-})
+
+    const newSubscription = new Subscription({
+      driver: driverId,
+      subscriptionType,
+    });
+
+    await newSubscription.save();
+    res.status(201).json(newSubscription);
+  } catch (error) {
+    console.error("Error creating subscription:", error);
+    res.status(500).json({ error: "Error creating subscription" });
+  }
+});
 
 module.exports = router;
