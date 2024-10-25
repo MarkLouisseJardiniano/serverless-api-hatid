56111486479c48fbc1710f2d0f82eb7324d1c47c
Mawi <mark@example.com>
2024-10-25 14:40:23 +0800

    add new status

--- functions/routes/ride.js
+++ functions/routes/ride.js
@@ -17,6 +17,8 @@ router.get("/booking", async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+
 router.get("/available", async (req, res) => {
   try {
     const { driverId } = req.query;
@@ -43,12 +45,17 @@ router.get("/available", async (req, res) => {
       status: { $in: ["accepted", "rejected"] }
     }).sort({ updatedAt: -1 });
 
-    // Build the query to find pending special and shared rides with "Create" action
+    // Build the base query to find pending rides
     const query = {
       status: "pending",
       vehicleType: vehicleType,
-      rideType: { $in: ["Special", "Shared Ride"] }, // Fetch only 'Special' and 'Shared' rides
+      rideType: { $in: ["Special", "Shared Ride"] }
     };
+
+    // Add the `rideAction` condition only for 'Shared Ride'
+    if (query.rideType.includes("Shared Ride")) {
+      query.rideAction = "Create";
+    }
 
     // If the driver has a current booking, exclude it
     if (currentBooking) {
@@ -65,6 +72,7 @@ router.get("/available", async (req, res) => {
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 });
+
 
 
 router.get("/available/shared", async (req, res) => {
