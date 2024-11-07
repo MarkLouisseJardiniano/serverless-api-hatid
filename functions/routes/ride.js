6ce646bce7947715b23398bd183f9bfbec0257c9
Mawi <mark@example.com>
2024-11-07 15:11:16 +0800

    add new status

--- functions/routes/ride.js
+++ functions/routes/ride.js
@@ -323,9 +323,7 @@ router.post("/accept-copassenger", async (req, res) => {
     // Update the status and fare of the new booking (co-passenger's booking)
     newBooking.status = "accepted";
     newBooking.fare = discountedFare; // Update the new booking's fare with the co-passenger discount
-
-    // Save the updated parent booking (with potential discount)
-    await parentBooking.save();
+    await newBooking.save(); // Save the updated new booking
 
     return res.status(200).json({
       status: "ok",
